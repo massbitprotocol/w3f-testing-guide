f9a3c23960c4d263159af1dc534ba54c760b4a98
hoanito <hhoang219@gmail.com>
2022-08-22 16:30:53 +0700

    Add unit test

--- 163/run_unit_test.sh
+++ 163/run_unit_test.sh
@@ -1,1 +1,1 @@
-cd /massbit/massbitroute/app/src/sites/services/api/;git checkout tags/v0.1.11;./scripts/run _install_test;./scripts/run _run_test
+cd /massbit/massbitroute/app/src/sites/services/api/;git fetch --all --tags;git checkout tags/v0.1.11;./scripts/run _install_test;./scripts/run _run_test
