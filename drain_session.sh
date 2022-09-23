b50217371a39e3739079c2746f6876252aa4512b
hoanito <hhoang219@gmail.com>
2022-09-23 18:59:37 +0700

    Chain issue need to be fixed

---
+++ drain_session.sh
@@ -0,0 +1,7 @@
+while true; do
+
+curl --location --request POST 'http://d74a9fe5-7f2c-44d2-9ba1-cad7b16b5d77.eth-mainnet.massbitroute.net/xdPEsv7WYMqhxnXNy4Imag' \
+--header 'Content-Type: application/json' \
+--data-raw '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+
+done
